70f44cc85019c253d02aa93bdded66698dd50bb9
b3rg0 <44955803+b3rg0@users.noreply.github.com>
2019-06-23 03:39:04 -0300

    Add files via upload

---
+++ SAA/ViewModels/MateriaViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace SAA.ViewModels{
+    class MateriaViewModel : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string materias;
+
+        public string Materias {
+            get { return materias; }
+            set {
+                materias = value;
+                if (PropertyChanged != null) {
+                    PropertyChanged(this, new PropertyChangedEventArgs("Personas"));
+                }
+            }
+        }
+
+        public MateriaViewModel() {
+            var listadoMaterias = SAA.App.BaseDeDatos.ObtenerMaterias();
+            foreach (SAA.Models.MateriaModel m in listadoMaterias) {
+                Materias += m.Nombre+"\n";
+            }
+        }
+    }
+}
