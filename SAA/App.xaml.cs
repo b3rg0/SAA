3c9b37258b83a5dfcda271355024e6d1de7c86a1
b3rg0 <44955803+b3rg0@users.noreply.github.com>
2019-06-22 21:13:09 -0300

    Add files via upload

---
+++ SAA/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace SAA {
+    public partial class App : Application {
+        public App() {
+            InitializeComponent();
+
+            MainPage = new Views.EditarView();
+        }
+
+        static Models.GestorBDD baseDeDatos;
+
+        public static Models.GestorBDD BaseDeDatos {
+            get {
+                if (baseDeDatos == null) {
+                    baseDeDatos = new Models.GestorBDD();
+                }
+                return baseDeDatos;
+            }
+        }
+        protected override void OnStart() {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep() {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume() {
+            // Handle when your app resumes
+        }
+    }
+}
