989ede042c4b5d01445579c9ef141455d2078c02
b3rg0 <44955803+b3rg0@users.noreply.github.com>
2019-06-26 17:04:17 -0300

    Add files via upload

--- SAA.Android/MainActivity.cs
+++ SAA.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Android.Widget;
 using Android.OS;
 
 namespace SAA.Droid{
-    [Activity(Label = "SAA", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "SAA", Icon = "@drawable/EasterEGG", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity {
         protected override void OnCreate(Bundle savedInstanceState){
             TabLayoutResource = Resource.Layout.Tabbar;
